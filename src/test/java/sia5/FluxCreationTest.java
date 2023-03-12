c32d0ad8ee2bd1167924f0c8a14ef75eb08691f1
yunyoung1819 <yunyoung1819@hanmail.net>
2023-03-13 03:07:03 +0900

    FluxCreate Test 코드 추가

--- src/test/java/sia5/FluxCreationTest.java
+++ src/test/java/sia5/FluxCreationTest.java
@@ -4,10 +4,14 @@ import org.junit.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
+/**
+ * 리액티브 타입 생성하기
+ */
 public class FluxCreationTest {
 	@Test
 	public void createAFlux_just() {
@@ -85,6 +89,21 @@ public class FluxCreationTest {
 				.expectNext(3)
 				.expectNext(4)
 				.expectNext(5)
+				.verifyComplete();
+	}
+
+	@Test
+	public void createAFlux_interval() {
+		Flux<Long> intervalFlux =
+				Flux.interval(Duration.ofSeconds(1))
+						.take(5);
+
+		StepVerifier.create(intervalFlux)
+				.expectNext(0L)
+				.expectNext(1L)
+				.expectNext(2L)
+				.expectNext(3L)
+				.expectNext(4L)
 				.verifyComplete();
 	}
 }
