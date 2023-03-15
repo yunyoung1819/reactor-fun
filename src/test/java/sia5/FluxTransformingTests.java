c2930e7b4edce8266c15d775982cfb4f71cadf6c
yunyoung1819 <yunyoung1819@hanmail.net>
2023-03-15 20:25:55 +0900

    리액티브 타입으로부터 데이터 필터링 테스트 코드 추가

---
+++ src/test/java/sia5/FluxTransformingTests.java
@@ -0,0 +1,95 @@
+package sia5;
+
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+/**
+ * 리액티브 타입으로부터 데이터 필터링하기
+ */
+public class FluxTransformingTests {
+
+	/**
+	 * skip() 오퍼레이션은 지정된 수의 메시지를 건너띈 후에
+	 * 나머지 메시지를 결과 Flux로 전달한다
+	 */
+	@Test
+	public void skipAFew() {
+		Flux<String> skipFlux = Flux
+				.just("one", "two", "skip a few", "ninety nine", "one hundred")
+				.skip(3);
+		StepVerifier.create(skipFlux)
+				.expectNext("ninety nine", "one hundred")
+				.verifyComplete();
+	}
+
+	/**
+	 * 이런 형태의 skip() 오퍼레이션은 지정된 시간이 경과할 때까지 기다렸다가
+	 * 결과 Flux로 메시지를 전달한다
+	 */
+	@Test
+	public void skipAFewSeconds() {
+		Flux<String> skipFlux = Flux.just(
+				"one", "two", "skip a few", "ninety nine", "one hundred")
+				.delayElements(Duration.ofSeconds(1))
+				.skip(Duration.ofSeconds(4));
+		StepVerifier.create(skipFlux)
+				.expectNext("ninety nine", "one hundred")
+				.verifyComplete();
+	}
+
+	/**
+	 * take() 오퍼레이션은 입력 Flux로부터 처음부터 지정된 수의 메시지만
+	 * 전달하고 구독을 취소시킨다
+	 */
+	@Test
+	public void take() {
+		Flux<String> cityFlux = Flux
+				.just("NewYork", "London", "Paris", "Tyokyo", "Seoul", "Beijing")
+				.take(3);
+		StepVerifier.create(cityFlux)
+				.expectNext("NewYork", "London", "Paris")
+				.verifyComplete();
+	}
+
+
+	/** 이런 형태의 take() 오퍼레이션은 일정 시간이 경과될 동안만
+	 * 결과 Flux로 메시지를 전달한다
+	 */
+	@Test
+	public void takeAFewSeconds() {
+		Flux<String> nationalParkFlux = Flux.just("Yellowstone", "Yosemite", "Grand Canyon", "Zion", "Grand Teton")
+				.delayElements(Duration.ofSeconds(1))
+				.take(Duration.ofMillis(3500));
+		StepVerifier.create(nationalParkFlux)
+				.expectNext("Yellowstone", "Yosemite", "Grand Canyon")
+				.verifyComplete();
+	}
+
+	/**
+	 * 지정된 조건식에 일치되는 메시지만 결과 Flux가 수신하도록 입력 Flux를 필터링 할 수 있다
+	 */
+	@Test
+	public void filter() {
+		Flux<String> nationalParkFlux = Flux
+				.just("Yellowstone", "Yosemite", "Grand Canyon", "Zion", "Grand Teton")
+				.filter(np -> !np.contains(" "));
+		StepVerifier.create(nationalParkFlux)
+				.expectNext("Yellowstone", "Yosemite", "Zion")
+				.verifyComplete();
+	}
+
+	/**
+	 * distinct() 오퍼레이션을 사용하면 발행된 적이 없는(중복되지 않는) 소스 Flux의 항목만 발행하는 결과 Flux를 생성한다
+	 */
+	@Test
+	public void distinct() {
+		Flux<String> animalFlux = Flux.just("dog", "cat", "bird", "dog", "bird", "anteater")
+				.distinct();
+		StepVerifier.create(animalFlux)
+				.expectNext("dog", "cat", "bird", "anteater")
+				.verifyComplete();
+	}
+}
